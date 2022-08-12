6089b4f5c98bdaad6c9805d1199e4e2e4ac581cf
Rohin Joshi <74340187+RohinJoshi1@users.noreply.github.com>
2022-08-12 16:15:50 +0530

    Update Heapsort.c

--- Practical_Exam/sem4/Heapsort.c
+++ Practical_Exam/sem4/Heapsort.c
@@ -1,59 +1,10 @@
-/*
-  @Override
-  public void sort(int[] values) {
-    Heapsort.heapsort(values);
-  }
-
-  private static void heapsort(int[] ar) {
-    if (ar == null) return;
-    int n = ar.length;
-
-    // Heapify, converts array into binary heap O(n), see:
-    // http://www.cs.umd.edu/~meesh/351/mount/lectures/lect14-heapsort-analysis-part.pdf
-    for (int i = Math.max(0, (n / 2) - 1); i >= 0; i--) {
-      sink(ar, n, i);
-    }
-
-    // Sorting bit
-    for (int i = n - 1; i >= 0; i--) {
-      swap(ar, 0, i);
-      sink(ar, i, 0);
-    }
-  }
-
-  private static void sink(int[] ar, int n, int i) {
-    while (true) {
-      int left = 2 * i + 1; // Left  node
-      int right = 2 * i + 2; // Right node
-      int largest = i;
-
-      // Right child is larger than parent
-      if (right < n && ar[right] > ar[largest]) largest = right;
-
-      // Left child is larger than parent
-      if (left < n && ar[left] > ar[largest]) largest = left;
-
-      // Move down the tree following the largest node
-      if (largest != i) {
-        swap(ar, largest, i);
-        i = largest;
-      } else break;
-    }
-  }
-
-  private static void swap(int[] ar, int i, int j) {
-    int tmp = ar[i];
-    ar[i] = ar[j];
-    ar[j] = tmp;
-  }
-*/
 #include <stdio.h>
 #define MAX(a,b) (((a)>(b))?(a):(b))
-void swap(int *xp, int *yp)
+void swap(int *a, int *b)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+    int temp = *a;
+    *a= *b;
+    *b = temp;
 }
  
 void sink(int arr[],int n,int i){
@@ -81,9 +32,6 @@ void heapsort(int arr[],int n){
    for (int i = MAX(0, (n / 2) - 1); i >= 0; i--) {
       sink(arr, n, i);
     }
-    // for(int i=0;i<n;i++){
-    //     printf("%d ",arr[i]);
-    // }
     //We have now constructed our heap 
     for(int i=n-1;i>=0;i--){
         swap(&arr[i],&arr[0]);
